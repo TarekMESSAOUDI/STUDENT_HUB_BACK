bfcee83820c146a8ae7d299a758449a63b258ee9
TarekMESSAOUDI <tarek.messaoudi@esprit.tn>
2022-04-08 23:09:07 +0100

    signin + signup

--- Models/RoleModel.js
+++ Models/RoleModel.js
@@ -1,8 +1,17 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 const Role = mongoose.model(
   "Role",
   new mongoose.Schema({
     nom: String
   })
 );
-module.exports = Role
+module.exports = Role*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roleModel = new Schema({
+  nom: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Role", roleModel);
