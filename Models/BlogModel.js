94a44eaca1b895117ced4c916d3ff49aad87df49
TarekMESSAOUDI <tarek.messaoudi@esprit.tn>
2022-06-02 14:18:25 +0100

    notif

--- Models/BlogModel.js
+++ Models/BlogModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const blogModel = new Schema({
   titre: { type: String },
   description: { type: String },
-  date: { type: String, default: new Date().toISOString() },
+  date: { type: String, default: new Date().toISOString().slice(0,10) },
   image: { type: String, default: "BLOG.jpeg", required: false },
   like: {type: Number, default: 0},
   masquer: {type: Boolean, default: true},
